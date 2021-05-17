e1dbcdf9e0e1cff7abf7228fad015669f3d6627a
Ari Wibowo <ari.wibowo@akarinti.tech>
2021-05-17 12:31:36 +0700

    first init

---
+++ src/main/java/com/example/springbootcrud/example/SpringBootCrudExampleApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootcrud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.util.ArrayUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class SpringBootCrudExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudExampleApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/springbootcrud/example/controller/UserController.java
@@ -0,0 +1,70 @@
+package com.example.springbootcrud.example.controller;
+
+import com.example.springbootcrud.example.entity.User;
+import com.example.springbootcrud.example.entity.UserDummy;
+import com.example.springbootcrud.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/user/add")
+    public String submitForm(@ModelAttribute("user") UserDummy user) {
+        User users = new User();
+        users.setName(user.getName());
+        users.setHp(user.getHp());
+        users.setEmail(user.getEmail());
+
+        String listString = "";
+
+        for (String s : user.getAlamat())
+        {
+            listString += s + "\t";
+        }
+
+        users.setAlamat(listString);
+        userService.saveUser(users);
+
+        return "";
+    }
+
+    @GetMapping("/user/list")
+    public String showUser(Model model) {
+        User user = new User();
+        List<User> listUser = new ArrayList<User>();
+        listUser = userService.getUser();
+
+        if (listUser == null || listUser.size() ==  0) {
+            return "";
+        }
+        else {
+            model.addAttribute("user", user);
+            model.addAttribute("users", listUser);
+            return "";
+        }
+    }
+
+    @PostMapping("/user/find")
+    public String submitSearchUser(@ModelAttribute("user") User user, Model model) {
+        List<User> listUser = new ArrayList<User>();
+        listUser =  userService.getUserByName(user.getName());
+
+        if (user.getName() == null || user.getName().equals("") ) {
+            model.addAttribute("users", userService.getUser());
+        }
+        else  {
+            model.addAttribute("users",listUser);
+        }
+
+        return "";
+    }
+
+}
